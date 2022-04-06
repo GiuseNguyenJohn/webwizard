67fbc31c66d75d17c597367a07d94e12b8e21436
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-04-05 19:17:35 -0700

    update HOW_TO_CONTRIBUTE and webwizard.py

---
+++ HOW_TO_CONTRIBUTE.md
@@ -0,0 +1,16 @@
+# How to Contribute
+
+## Master List of Tasks
+
+- [] Create class 'client' with parameters: URL, PORT=443 (default)
+    - [] 
+    - [] 
+    - [] 
+    - [] 
+    - [] 
+    - [] 
+    - [] 
+    - [] 
+    - [] 
+    - [] 
+    - [] 
--- project/webwizard.py
+++ project/webwizard.py
@@ -3,5 +3,6 @@
 A python module to aid and automate CTF web challenges.
 
 Authors: John (@Magicks52), David (@DavidTimothyNam)
-Tested: Python 3.9 on Kali Linux
+Tested: Python 3.9 on Kali Linux and Python _ on Ubuntu TODO: (David) fill in python and ubuntu version numbers
 """
+
