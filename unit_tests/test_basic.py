f98f68e1ceae2fe370ce9f17e4dc4b6f04977387
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-04-06 19:18:56 -0700

    make example_text one line

--- unit_tests/test_basic.py
+++ unit_tests/test_basic.py
@@ -5,8 +5,7 @@ import webwizard
 
 class TestBasic(unittest.TestCase):
     def test_parse_for_flag(self):
-        example_text = 'al;kdjlf.s@#skadlkfjlak233219f'
-        'lsakdctf{flag}laskdj@#$/dfjaos'
+        example_text = 'al;kdjlf.s@#skadlkfjlak233219flsakdctf{flag}laskdj@#$/dfjaos'
         flags = webwizard.parse_for_flag('ctf{', example_text)
         self.assertIn('ctf{flag}', flags)
 
