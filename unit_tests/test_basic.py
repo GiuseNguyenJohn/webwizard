d1a99718e83e62dedc2928c3bb6593a332e82956
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-04-11 11:31:35 -0700

    updated makefile, create new basic unit test

--- unit_tests/test_basic.py
+++ unit_tests/test_basic.py
@@ -1,10 +1,15 @@
 """Tests for webwizard.py core functionality."""
 
+import tempfile
 import unittest
 import webwizard
 
 class TestBasic(unittest.TestCase):
+    """Test basic features of webwizard"""
+
     def test_parse_for_flag(self):
+        """Test plaintext, rot13, and base64 flag detection"""
+
         example_text = 'al;kdjlf.s9flsakdctf{flag}laskdj@#$/dfjaos'
         flags = webwizard.parse_for_flag('ctf{', example_text)
         self.assertIn('plaintext flag: ctf{flag}', flags)
@@ -15,5 +20,12 @@ class TestBasic(unittest.TestCase):
         flags = webwizard.parse_for_flag('ctf{', example_text)
         self.assertIn('base64 flag: ctf{flag}', flags)
         
+    def test_get_files_in_dir(self):
+        """Will passing the directory ./test/file1, ./test/file2
+        return the correct paths?
+        """
+
+        os.mkdir('./test')
+
 if __name__ == "__main__":
     unittest.main()
