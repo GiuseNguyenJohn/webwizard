b1604f2b6ea5c5174e8a1e275d0118c89a190121
John Nguyen <72513899+Magicks52@users.noreply.github.com>
2022-04-15 12:43:59 -0700

    fixed parsing for flag methods

--- src/webwizard.py
+++ src/webwizard.py
@@ -54,9 +54,9 @@ def get_files_in_dir(path_to_directory: str) -> list:
 def parse_file_for_flag(crib: str, file_path: str) -> list:
     """Parses file for crib. Assumes file has valid utf-8 bytes."""
 
-    with open(file_path, 'rb'):
+    with open(file_path, 'rb') as f:
         # ignore bad utf-8 characters
-        flags = parse_for_flag(crib, file_path.read().decode("utf-8", "ignore"))
+        flags = parse_for_flag(crib, f.read().decode("utf-8", "ignore"))
     return flags
 
 
@@ -283,5 +283,5 @@ class Client:
         # parse all subfiles for flag
         flags = []
         for subfile in subfile_list:
-            flags += parse_file_for_flag(subfile)
+            flags += parse_file_for_flag(crib, subfile)
         return flags
